6e1bbe8785e8a9d5db680e0a4f31d94e5911f1b4
D_ttang <D_ttang@163.com>
2015-08-04 00:13:38 +0800

    register

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -19,22 +19,39 @@ protocol LoginViewControllerDelegate : class{
 class LoginViewController: UIViewController {
     
     weak var delegate: LoginViewControllerDelegate?
-    
-    
+
+    @IBOutlet weak var dismissBtn: UIButton!
     @IBOutlet weak var userName: MKTextField!
     @IBOutlet weak var password: MKTextField!
+    @IBOutlet weak var conformPassword: MKTextField!
+    
     @IBOutlet weak var registerBtn: UIButton!
     @IBOutlet weak var loginBtn: UIButton!
     
     @IBOutlet weak var loginBtnWidth: NSLayoutConstraint!
+    @IBOutlet weak var loginBtnTop: NSLayoutConstraint!
+    @IBOutlet weak var registerBtnTop: NSLayoutConstraint!
+    @IBOutlet weak var registerBtnWidth: NSLayoutConstraint!
+    
     @IBOutlet var loginRight: NSLayoutConstraint!
+    @IBOutlet var registerLeft: NSLayoutConstraint!
+    
+    @IBOutlet var conformCenter: NSLayoutConstraint!
+    @IBOutlet weak var conformLeft: NSLayoutConstraint!
     
     var centerLayoutConstraint : NSLayoutConstraint?
-    
+
+    var registerText: String?
     let ovalShapeLayer: CAShapeLayer = CAShapeLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dismissBtn.setTitle(GoogleIcon.e811, forState: UIControlState.Normal)
+        dismissBtn.tintColor = UIColor.whiteColor()
+        dismissBtn.layer.cornerRadius = CGRectGetHeight(dismissBtn.frame) / 2
+        
+        conformFeildHidden()
         
         initElement()
     }
@@ -42,20 +59,26 @@ class LoginViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         self.userName.becomeFirstResponder()
     }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func dismissView(sender: AnyObject) {
-        
+    
+    
+    func login(){
         let userNameValue = userName.text!
         let passwordValue = password.text!
         self.loginBtn.enabled = false
         
         self.view.removeConstraint(self.loginRight)
-
+        
         
         centerLayoutConstraint = NSLayoutConstraint(item: self.loginBtn, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0)
         self.view.addConstraint(self.centerLayoutConstraint!)
@@ -63,38 +86,70 @@ class LoginViewController: UIViewController {
         self.loginBtn.setTitle("", forState: UIControlState.Normal)
         
         UIView.animateWithDuration(0.5, animations: {
-                self.registerBtn.alpha = 0
-                self.view.layoutIfNeeded()
+            self.registerBtn.alpha = 0
+            self.view.layoutIfNeeded()
             }, completion: {
                 _ in
-                self.addMMSpinner()
+                self.addMMSpinner(self.loginBtn)
                 let requestBody = ["userName": userNameValue, "password": passwordValue]
-                self.sendUserNameAndPwd("http://107.150.96.151/api/me/login", requestBody: requestBody)
+                self.sendUserNameAndPwd("http://107.150.96.151/api/me/login", requestBody: requestBody, isLogin: true)
         })
-        
     }
     
-    func sendUserNameAndPwd(url: String, requestBody: [String: String]){
+    func register(){
+        let userNameValue = userName.text!
+        let passwordValue = password.text!
+        let conformedPassword = conformPassword.text!
+        
+        if conformedPassword != passwordValue {
+            self.displayAlert("两次输入的密码不一样")
+            return
+        }
+        
+        self.registerBtn.enabled = false
+        
+        self.view.removeConstraint(self.registerLeft)
+        
+        
+        centerLayoutConstraint = NSLayoutConstraint(item: self.registerBtn, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0)
+        self.view.addConstraint(self.centerLayoutConstraint!)
+        self.registerBtnWidth.constant = self.registerBtn.frame.height
+        self.registerBtn.setTitle("", forState: UIControlState.Normal)
+        
+        UIView.animateWithDuration(0.5, animations: {
+            self.loginBtn.alpha = 0
+            self.view.layoutIfNeeded()
+            }, completion: {
+                _ in
+                self.addMMSpinner( self.registerBtn)
+                let requestBody = ["userName": userNameValue, "password": passwordValue]
+                self.sendUserNameAndPwd("http://107.150.96.151/api/me/login", requestBody: requestBody, isLogin: false)
+        })
+    }
+    
+    func sendUserNameAndPwd(url: String, requestBody: [String: String], isLogin: Bool){
         MyHTTPHandler.post(url, params: requestBody){
             
             data, err in
             dispatch_async(dispatch_get_main_queue(), {
-                // code here
+
                 let jsonParsed: AnyObject!
                 if err != nil {
                     self.displayAlert("client goes wrong")
                     return
                 }
                 
-//                let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
-//                print("\(strData)")
                 do {
                     jsonParsed = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions())
-                    // print("json\(jsonParsed)")
                 }catch _ {
                     print("err")
                     self.displayAlert("parse json goes wrong")
-                    self.resumeLoginBtn()
+                    if isLogin {
+                        self.resumeLoginBtn()
+                    }else {
+                        self.resumeRegisterBtn()
+                    }
+
                     return
                 }
                 
@@ -104,7 +159,11 @@ class LoginViewController: UIViewController {
                     self.logined()
                 }else {
                     self.displayAlert("password is wrong")
-                    self.resumeLoginBtn()
+                    if isLogin {
+                        self.resumeLoginBtn()
+                    }else {
+                        self.resumeRegisterBtn()
+                    }
                 }
             })
             
@@ -119,8 +178,28 @@ class LoginViewController: UIViewController {
         self.presentViewController(alertController, animated: true, completion: nil)
     }
     
-    func resumeLoginBtn(){
+    func resumeRegisterBtn(){
     
+        self.ovalShapeLayer.removeFromSuperlayer()
+        self.view.removeConstraint(self.centerLayoutConstraint!)
+        self.view.addConstraint(self.registerLeft)
+        
+        self.registerBtnWidth.constant = 120
+        self.registerBtn.enabled = true
+        
+        UIView.animateWithDuration(0.1, animations: {
+            
+            self.loginBtn.alpha = 1
+            self.view.layoutIfNeeded()
+            
+            }, completion: {
+                _ in
+                self.registerBtn.setTitle("ok", forState: UIControlState.Normal)
+        })
+    }
+    
+    func resumeLoginBtn(){
+        
         self.ovalShapeLayer.removeFromSuperlayer()
         self.view.removeConstraint(self.centerLayoutConstraint!)
         self.view.addConstraint(self.loginRight)
@@ -134,7 +213,7 @@ class LoginViewController: UIViewController {
             
             }, completion: {
                 _ in
-                self.loginBtn.setTitle("Login", forState: UIControlState.Normal)
+                self.loginBtn.setTitle("登陆", forState: UIControlState.Normal)
         })
     }
     
@@ -147,8 +226,8 @@ class LoginViewController: UIViewController {
         self.userName.becomeFirstResponder()
         self.userName.resignFirstResponder()
     }
+    
     func initElement(){
-        
         loginBtn.layer.cornerRadius = CGRectGetHeight(self.loginBtn.frame) / 2
         registerBtn.layer.cornerRadius = CGRectGetHeight(self.registerBtn.frame) / 2
         
@@ -160,7 +239,6 @@ class LoginViewController: UIViewController {
         userName.cornerRadius = 0
         userName.bottomBorderEnabled = true
         
-        
         password.layer.borderColor = UIColor.clearColor().CGColor
         password.floatingPlaceholderEnabled = true
         password.placeholder = "password"
@@ -168,22 +246,66 @@ class LoginViewController: UIViewController {
         password.rippleLocation = .Right
         password.cornerRadius = 0
         password.bottomBorderEnabled = true
+        
+        conformPassword.layer.borderColor = UIColor.clearColor().CGColor
+        conformPassword.floatingPlaceholderEnabled = true
+        conformPassword.placeholder = "conform password"
+        conformPassword.tintColor = UIColor.MKColor.BlueGrey
+        conformPassword.rippleLocation = .Right
+        conformPassword.cornerRadius = 0
+        conformPassword.bottomBorderEnabled = true
     }
     
     @IBAction func registerAction(sender: AnyObject) {
+        registerText = registerBtn.currentTitle
+        if registerText == "注册" {
+            conformFeildShow()
+            UIView.animateWithDuration(0.5, animations: {
+                self.view.layoutIfNeeded()
+                self.registerBtn.setTitle("ok", forState: UIControlState.Normal)
+                self.loginBtn.setTitle("cancel", forState: UIControlState.Normal)
+                self.loginBtn.tintColor = UIColor.whiteColor()
+                self.loginBtn.backgroundColor = UIColor.redColor()
+            })
 
+        }
+        if registerText == "ok" {
+            //register
+            self.register()
+        }
+    }
+    
+    @IBAction func loginAction(sender: AnyObject) {
+        if loginBtn.currentTitle == "登陆" {
+            self.login()
+        }
+        
+        if loginBtn.currentTitle == "cancel" {
+            conformFeildHidden()
+            UIView.animateWithDuration(0.5, animations: {
+                self.view.layoutIfNeeded()
+                self.registerBtn.setTitle("注册", forState: UIControlState.Normal)
+                self.loginBtn.setTitle("登陆", forState: UIControlState.Normal)
+                self.loginBtn.tintColor = self.registerBtn.tintColor
+                self.loginBtn.backgroundColor = self.registerBtn.backgroundColor
+            })
+        }
     }
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func conformFeildHidden(){
+        view.removeConstraint(conformCenter)
+        conformLeft.constant -= 500
+        loginBtnTop.constant -= 60
+        registerBtnTop.constant -= 60
     }
-    */
+    
+    func conformFeildShow(){
+        view.addConstraint(conformCenter)
+        conformLeft.constant += 500
+        loginBtnTop.constant += 60
+        registerBtnTop.constant += 60
+    }
     
     
     @IBAction func cancelAction(sender: AnyObject) {
@@ -194,23 +316,23 @@ class LoginViewController: UIViewController {
 
 extension LoginViewController {
     
-    func addMMSpinner(){
+    func addMMSpinner(superView: UIView){
         
         ovalShapeLayer.strokeColor = UIColor.whiteColor().CGColor
         ovalShapeLayer.fillColor = UIColor.clearColor().CGColor
         ovalShapeLayer.lineWidth = 2.0
-        ovalShapeLayer.frame = CGRectMake(0, 0, self.loginBtn.frame.width, self.loginBtn.frame.height)
+        ovalShapeLayer.frame = CGRectMake(0, 0, superView.frame.width, superView.frame.height)
         //        ovalShapeLayer.backgroundColor = UIColor.orangeColor().CGColor
-        let refreshRadius = self.loginBtn.frame.size.height/2 * 0.5
+        let refreshRadius = superView.frame.size.height/2 * 0.5
         let path = UIBezierPath(ovalInRect: CGRect(
-            x: self.loginBtn.frame.size.width/2 - refreshRadius,
-            y: self.loginBtn.frame.size.height/2 - refreshRadius,
+            x: superView.frame.size.width/2 - refreshRadius,
+            y: superView.frame.size.height/2 - refreshRadius,
             width: 2 * refreshRadius,
             height: 2 * refreshRadius))
         
         ovalShapeLayer.path = path.CGPath
         
-        self.loginBtn.layer.addSublayer(ovalShapeLayer)
+        superView.layer.addSublayer(ovalShapeLayer)
         beginRefreshing()
     }
     
